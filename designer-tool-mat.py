b4e8aaf37441b07ad27055ea749ab3ee3a08cb41
Aman Singh <amansingh844123@gmail.com>
2025-01-17 01:02:08 +0530

    DAY-3: add designer-tool-mat.py for generating a patterned door mat with 'WELCOME' in the center

---
+++ designer-tool-mat.py
@@ -0,0 +1,68 @@
+"""
+PROBLEM STATEMENT:
+
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be X. ( is an odd natural number, and  is  times .)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+
+Input Format
+A single line containing the space separated values of  and .
+
+
+Sample Input
+9 27
+
+Sample Output
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+
+"""
+
+# Solution
+
+# taking input from user and mapping it into 2 variables
+n, m = map(int, input().split())
+
+# first I need to print the top part
+for i in range(1, n, 2):
+    print((".|." * i).center(m, "-"))
+    
+# now need to print in center the "welcome" text
+print("WELCOME".center(m, "-"))
+
+# now print the top part in the reverse formate
+for i in range(n-2, 0, -2):
+    print((".|." * i).center(m,"-"))
+    
